d64c8237bbaf035efa52dafd0f2ba23047fc7f3d
mayurkas <mayurkas@amdocs.com>
2020-11-17 19:35:29 +0530

    add auth-dto

--- nestjs-task-manager/src/auth/user.entity.ts
+++ nestjs-task-manager/src/auth/user.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
 
     @PrimaryGeneratedColumn()
@@ -10,5 +11,8 @@ export class User extends BaseEntity {
     username: string;
 
     @Column()
-    password: string
+    password: string;
+
+    @Column()
+    salt: string
 }
--- nestjs-task-manager/src/auth/user.repository.ts
+++ nestjs-task-manager/src/auth/user.repository.ts
@@ -1,7 +1,34 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm"
+import * as bcrypt from 'bcryptjs';
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const user = new User();
+
+        user.username = username;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+        console.log(user.salt);
+
+        try{
+            await user.save();
+        } catch(error) {
+            if(error.code === '23505') { // duplicate username
+                throw new ConflictException('Username already exists')
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password,salt);
+    }
 }
